6259d5ab37aea6efeed05ceb799cdb860e2f35e5
Poornima3787-developer <taehyungpurple02@gmail.com>
2026-03-09 17:14:10 +0530

    completed the structure

---
+++ android/app/src/main/kotlin/com/example/todolist_dashboard/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.todolist_dashboard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/homeScreen.dart
@@ -0,0 +1,260 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget{
+  Widget categoryCard(IconData icon,String title,String task,Color color){
+    return Container(
+      width:119,
+      height:137,
+      padding:EdgeInsets.all(10),
+      decoration:BoxDecoration(
+        color:color,
+        borderRadius:BorderRadius.circular(15),
+      ),
+      child:Column(
+        crossAxisAlignment:CrossAxisAlignment.start,
+        children:[
+          Icon(icon,color:Colors.white,size:28),
+          Spacer(),
+          Text(
+            title,
+            style:TextStyle(
+              color:Colors.white,
+              fontWeight:FontWeight.bold,
+              fontSize:16,
+            ),
+          ),
+          SizedBox(height:4),
+          Text(task,style:TextStyle(
+            color:Colors.white70,
+            fontSize:12,
+          ),
+          ),
+          Icon(Icons.add,size:16,color:Colors.white),
+        ],
+      ),
+    );
+  }
+
+ Widget taskItem(IconData icon,String title,String tasks,String taskNumber){
+  return Padding(
+    padding:EdgeInsets.symmetric(horizontal:30),
+    child:Row(
+      children:[
+        Container(width:40,
+        height:40,
+        decoration:BoxDecoration( color:Colors.purple.shade50,),
+        child:Icon(icon,color:Colors.purple),
+        ),
+        SizedBox(width:20),
+        Expanded(
+          child:Column(
+            crossAxisAlignment:CrossAxisAlignment.start,
+            children:[
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize:16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                tasks,style:TextStyle(
+                  fontSize:11,color:Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+                padding:EdgeInsets.symmetric(horizontal:8,vertical:4),
+                decoration:BoxDecoration(
+                  color:Colors.purple.shade50,
+                ),
+                child:Text(taskNumber,style:TextStyle(fontSize:11,color:Colors.purple,),),
+              ),
+      ],
+    ),
+  );
+ }
+
+  Widget build(BuildContext context){
+    return Scaffold(
+      floatingActionButton:FloatingActionButton(
+        onPressed:() {},
+        backgroundColor:Colors.purple,
+        child:Icon(Icons.add),
+      ),
+      body: Stack(
+  children: [
+
+    // Background
+    Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.deepPurple,
+            Colors.purpleAccent,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+    ),
+
+    // Top content
+    SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(height: 60),
+
+            // Hello + profile
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Hello",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22,
+                        )),
+                    Text("Jhon Doe",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ],
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.asset(
+                    "assets/images/profile.png",
+                    width: 57,
+                    height: 57,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 20),
+
+            // Search bar
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.search, color: Colors.grey),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Search....",
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  Icon(Icons.tune, color: Colors.grey),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 30),
+
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Category",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 23,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  categoryCard(Icons.brush, "Design", "Tasks 5", Colors.pink),
+                  SizedBox(width: 10),
+                  categoryCard(Icons.meeting_room, "Meeting", "Tasks 2", Colors.orange),
+                  SizedBox(width: 10),
+                  categoryCard(Icons.school, "Learning", "Tasks 1", Colors.green),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 300), // space behind sheet
+          ],
+        ),
+      ),
+    ),
+
+    // Bottom draggable sheet
+    DraggableScrollableSheet(
+      initialChildSize: 0.4,
+      minChildSize: 0.35,
+      maxChildSize: 0.9,
+      builder: (context, scrollController) {
+        return Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(25),
+            ),
+          ),
+          child: ListView(
+            controller: scrollController,
+            padding: EdgeInsets.all(20),
+            children: [
+              Center(
+                child: Container(
+                  width: 40,
+                  height: 4,
+                  margin: EdgeInsets.only(bottom: 20),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+
+              Text(
+                "Today's Task",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              SizedBox(height: 20),
+
+              taskItem(Icons.brush, "Sketching","completed 2","2"),
+              SizedBox(height:10),
+              taskItem(Icons.crop_square, "WireFraming","completed 0","4"),
+              SizedBox(height:10),
+              taskItem(Icons.palette, "Visual Design","completed 1","3"),
+            ],
+          ),
+        );
+      },
+    ),
+  ],
+),
+);
+}
+}
