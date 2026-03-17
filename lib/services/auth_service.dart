27f9327244b1fac622cebaee7828fb58aa04ac05
Poornima <taehyungpurple02@gmail.com>
2026-03-17 14:22:14 +0530

    Signup screen

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class AuthService {
-  Future login(String email, String password) async {
+  Future signup(String email, String password) async {
     final response = await http.post(
       Uri.parse("http://localhost:3000/login"),
       headers: {'Content-Type': "application/json"},
